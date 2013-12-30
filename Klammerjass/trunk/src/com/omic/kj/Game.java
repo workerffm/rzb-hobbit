96d8b81ff79ef5e30fe3ff3ad1bf017ca340e1b2
arbeiter.markus <arbeiter.markus@gmail.com>
2013-12-30 00:21:08 +0000

    Game FSM reworked

--- Klammerjass/trunk/src/com/omic/kj/Game.java
+++ Klammerjass/trunk/src/com/omic/kj/Game.java
@@ -1,11 +1,13 @@
 package com.omic.kj;
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import com.omic.kj.shared.domain.*;
 
 final class Game {
-	
+
 	private final Logger log = Logger.getLogger("Game");
 
 	private int gameId; // identifier for compare method
@@ -14,7 +16,7 @@ final class Game {
 	private int round; // Runde / Stich Nummer
 	private String teamName;
 	private Date start, end; // game duration
-	private List<PlayCard> cards;    // 32 cards with special tags on it
+	private List<PlayCard> cards; // 32 cards with special tags on it
 	private Farbe trumpf; // Trumpffarbe
 	private Player geber; // Kartengeber
 	private Player mainplayer; // Spieler/Gewinner beim "Reizen"
@@ -22,11 +24,16 @@ final class Game {
 	private Player roundFirstPlayer; // Spieler der beim aktuellen Stich rausgekommen ist / rauskommen muß
 	private Player currentPlayer; // Der Spieler ist grad an der Reihe
 	private Karte roundCard; // Zuerst ausgespielte Karte in aktueller Runde, diese Farbe muß bedient werden.
+	private Karte original;
+
 	private CommandListener commandListener; // send player command to this listener!
-	
-	private final Set<Player> player; // 2 to 4 players per game
+	private ArrayBlockingQueue<PlayerResponse> responseQueue;
+
+	private final List<Player> player; // 2 to 4 players per game
 	private final Set<ComputerPlayer> computerPlayer; // save extra the auto player
 	private final List<PlayRound> rounds;
+
+	private GameState state;
 
 	/*             Spiel        | Runde
 	 * ----------------------------------------
@@ -37,9 +44,10 @@ final class Game {
 	 */
 
 	public Game() {
-		this.player = new HashSet<>();
+		this.player = new ArrayList<>();
 		this.computerPlayer = new HashSet<>();
 		this.rounds = new ArrayList<>();
+		this.responseQueue = new ArrayBlockingQueue<>(8);
 	}
 
 	public void joinGame(final Player p, final GameSettings gs) throws Exception {
@@ -76,24 +84,168 @@ final class Game {
 
 	public void startGame() throws Exception {
 		int n = player.size();
-		if (n < 2){
+		if (n < 2) {
 			throw new Exception("Zu wenig Spieler für ein Spiel.");
 		}
 		if (n > 4) {
 			throw new Exception("Zu viele Spieler für ein Spiel.");
 		}
 
+		loadCards();
+
 		this.start = new Date();
 		this.teamName = "Team X";
-		this.gameName = "Game X";
+		this.gameName = "Game " + gameId;
 		this.round = 1;
-		
-		sayHello();
-		loadCards();
-		selectGeber();
-		geben();
+		this.state = GameState.G0;
+		final int playerCount = player.size();
+
+		if (playerCount < 2 || playerCount > 4) {
+			throw new Exception("Unpassende Spieleranzahl.");
+		}
+
+		// LOOP FOR 4 PLAYER !!
+
+		for (;;) {
+
+			final PlayerResponse response = getNextPlayerResponse(2000);
+			log.info("State=" + this.state + ", response=" + response);
+
+			switch (this.state) {
+
+			// -----------------------------------------------------------
+			case G0: {
+				selectGeber();
+				// 3 Karten für jeden
+				for (int i = 0; i < 3; i++) {
+					for (Player p : player) {
+						assignFreeCardToPlayer(p, false);
+					}
+				}
+				sendPlayerInfo();
+				this.state = GameState.G1;
+				break;
+			}
+
+			// -----------------------------------------------------------
+			case G1: {
+				// 2 Karten für jeden
+				this.original = null;
+				for (int i = 0; i < 2; i++) {
+					for (Player p : player) {
+						assignFreeCardToPlayer(p, p.getId() == this.geber.getId());
+					}
+				}
+				sendPlayerInfo();
+				this.state = GameState.G2;
+				break;
+			}
+			// -----------------------------------------------------------
+			case G2: {
+				Player p = getPlayerFromGeber(+1);
+				sendPlayerCommand(p, Command.frageOriginal);
+				this.state = GameState.G3;
+				//
+				break;
+			}
+			// -----------------------------------------------------------
+			case G3: {
+				if (response != null) {
+
+				}
+				break;
+			}
+			// -----------------------------------------------------------
+			case G4: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G5: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G6: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G7: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G8: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G9: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G10: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G11: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G12: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G13: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case G14: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case S0: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case S1: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case X2: {
+				break;
+			}
+			// -----------------------------------------------------------
+			case X4: {
+				break;
+			}
+			}
+		}
+		//sayHello();
+		//selectGeber();
+		//geben();
 		//	GameLogger.printGame(game);
 		//	startRounds();
+	}
+
+	private void sendPlayerCommand(Player p, Command frageoriginal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * n-ter Spieler im Uhrzeigersinn vom Geber ausgehend
+	 * =REST(A2-1+$B$1;4)+1
+	 */
+	private Player getPlayerFromGeber(int step) throws Exception {
+		int nextPosition = ((this.geber.getPosition() - 1 + step) % player.size()) + 1;
+		for (Player p : player) {
+			if (p.getPosition() == nextPosition)
+				return p;
+		}
+		throw new Exception("next player not found");
+	}
+
+	private void sendPlayerInfo() {
+		for (Player p : player) {
+			sendPlayerInfo(p);
+		}
 	}
 
 	/**
@@ -103,54 +255,54 @@ final class Game {
 	public void selectGeber() {
 		this.geber = player.iterator().next();
 	}
-	
-	private void geben() throws Exception {
-		//Set<Player> player = game.getPlayers();
-		if (player.size() == 4)
-			geben4();
-		else if (player.size() == 2 || player.size() == 3)
-			geben2();
-		else
-			throw new Exception("Unpassende Spieleranzahl.");
-	}
 
-		private void geben2() {
-		
-	}
+	//	private void geben() throws Exception {
+	//		//Set<Player> player = game.getPlayers();
+	//		if (player.size() == 4)
+	//			geben4();
+	//		else if (player.size() == 2 || player.size() == 3)
+	//			geben2();
+	//		else
+	//			throw new Exception("Unpassende Spieleranzahl.");
+	//	}
+	//
+	//	private void geben2() {
+	//
+	//	}
 
-		/**
-		* Kartenverteilregel für 4 Spieler
-		* @throws GameException 
-		*/ 
-		private void geben4() {
-			// 3 Karten für jeden
-			for (int i = 0; i < 3; i++) {
-				for (Player p : player) {
-					assignFreeCardToPlayer(p);
-					sendPlayerInfo(p);
-				}
-			}
-			// 2 Karten für jeden
-			for (int i = 0; i < 2; i++) {
-				for (Player p : player) {
-					assignFreeCardToPlayer(p);
-					sendPlayerInfo(p);
-				}
-			}
-//			boolean ok = pli(geber).askOriginal(game);
-//			if (ok) {
-//				// 3 Karten für jeden
-//				for (int i = 0; i < 3; i++) {
-//					for (Player p : player) {
-//						assignFreeCardToPlayer(p);
-//						sendPlayerInfo(p);
-//					}
-//				}
-//			}
-//			else{
-//				throw new GameOverException("Kein Trumpf gewählt.");
-//			}
-		}
+	//	/**
+	//	* Kartenverteilregel für 4 Spieler
+	//	* @throws GameException 
+	//	*/
+	//	private void geben4() {
+	//		//		// 3 Karten für jeden
+	//		//		for (int i = 0; i < 3; i++) {
+	//		//			for (Player p : player) {
+	//		//				assignFreeCardToPlayer(p);
+	//		//				sendPlayerInfo(p);
+	//		//			}
+	//		//		}
+	//		//		// 2 Karten für jeden
+	//		//		for (int i = 0; i < 2; i++) {
+	//		//			for (Player p : player) {
+	//		//				assignFreeCardToPlayer(p);
+	//		//				sendPlayerInfo(p);
+	//		//			}
+	//		//		}
+	//		//			boolean ok = pli(geber).askOriginal(game);
+	//		//			if (ok) {
+	//		//				// 3 Karten für jeden
+	//		//				for (int i = 0; i < 3; i++) {
+	//		//					for (Player p : player) {
+	//		//						assignFreeCardToPlayer(p);
+	//		//						sendPlayerInfo(p);
+	//		//					}
+	//		//				}
+	//		//			}
+	//		//			else{
+	//		//				throw new GameOverException("Kein Trumpf gewählt.");
+	//		//			}
+	//	}
 
 	private void sendPlayerInfo(final Player p) {
 		final PlayerCommand command = new PlayerCommand();
@@ -163,7 +315,7 @@ final class Game {
 
 	private PlayerInfo buildPlayerInfo(final Player p) {
 		final PlayerInfo i = new PlayerInfo();
-		i.setAufspieler(roundFirstPlayer!=null?roundFirstPlayer.getUsername():"");
+		i.setAufspieler(roundFirstPlayer != null ? roundFirstPlayer.getUsername() : "");
 		i.setGameName(gameName);
 		i.setGeber(geber.getUsername());
 		i.setKarten(getCardsOnHand(p));
@@ -179,14 +331,14 @@ final class Game {
 
 	private Set<Handkarte> getCardsOnHand(Player p) {
 		final Set<Handkarte> playercards = new HashSet<>();
-		for (PlayCard pc:cards){
-			if(pc.getOwner()!=null && pc.getOwner().getId()==p.getId()) {
+		for (PlayCard pc : cards) {
+			if (pc.getOwner() != null && pc.getOwner().getId() == p.getId()) {
 				Handkarte hc = new Handkarte();
 				hc.setKarte(pc.getKarte());
 				hc.setOffen(!p.isComputer());
-			  playercards.add(hc);
+				playercards.add(hc);
 			}
-		} 
+		}
 		return playercards;
 	}
 
@@ -200,23 +352,25 @@ final class Game {
 		}
 	}
 
-	
-	private void assignFreeCardToPlayer(final Player p) {
-	for(;;) {
-		long skip = Math.round(Math.random()*61), s=0;
-		for (final PlayCard c : cards) {
-			if (s++ >= skip) {
-				if (c.isFree()) {
-					c.setOwner(p);
-					log.finer ("card assigned: card="+c.getKarte()+", player="+p.getId());
-					return;
+	private void assignFreeCardToPlayer(final Player p, boolean assignOriginal) {
+		for (;;) {
+			long skip = Math.round(Math.random() * 61), s = 0;
+			for (final PlayCard c : cards) {
+				if (s++ >= skip) {
+					if (c.isFree()) {
+						c.setOwner(p);
+						log.info("card assigned: card=" + c.getKarte() + ", player=" + p.getId());
+						if (assignOriginal && this.original == null) {
+							this.original = c.getKarte();
+							log.info("original assigned: card=" + c.getKarte());
+						}
+						return;
+					}
 				}
 			}
 		}
 	}
-	}
 
-	
 	/**
 	* Karten frisch initialisieren.
 	*/
@@ -233,25 +387,24 @@ final class Game {
 		this.cards = playCards;
 	}
 
-	public void stopGame() {
-		// TODO: 
+	public void setId(int id) {
+		this.gameId = id;
 	}
 
-//	public Set<Player> getPlayers() {
-//		return player;
-//	}
-
-	public void onPlayerResponse(PlayerResponse response) {
-		// TODO add queue and thread
-
+	public void stopGame() {
+		// TODO: 
 	}
 
 	public void setCommandListener(CommandListener commandListener) {
 		this.commandListener = commandListener;
 	}
 
-	public void setId(int id) {
-		this.gameId = id;
+	public void onPlayerResponse(PlayerResponse response) {
+		this.responseQueue.add(response);
+	}
+
+	private PlayerResponse getNextPlayerResponse(int timeout_ms) throws InterruptedException {
+		return this.responseQueue.poll(timeout_ms, TimeUnit.MILLISECONDS);
 	}
 
 }
